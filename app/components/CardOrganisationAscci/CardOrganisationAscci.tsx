2ec401a67d2d8cb1ab99ce0615222a6076f0ed76
fabriceadou5@gmail.com <fabriceadou5@gmail.com>
2025-05-15 22:53:45 +0000

    Organisation

--- app/components/CardOrganisationAscci/CardOrganisationAscci.tsx
+++ app/components/CardOrganisationAscci/CardOrganisationAscci.tsx
@@ -75,7 +75,7 @@ const CardOrganisationAscci = () => {
       <div className="lg:bg-white lg:h-[100vh]">
         <div className="p-8">
           {/* Afficher  */}
-          <div className="hidden lg:flex lg:flex-row justify-between items-center gap-2 text-black font-extrabold">
+          {/* <div className="hidden lg:flex lg:flex-row justify-between items-center gap-2 text-black font-extrabold">
             {Object.keys(CONTENT_COMPONENTS).map((id) => (
               <Link
                 key={id}
@@ -93,10 +93,10 @@ const CardOrganisationAscci = () => {
                 {id.split("-").join(" ")}
               </Link>
             ))}
-          </div>
+          </div> */}
 
           {/* Version mobile - Liste déroulante */}
-          <div className="w-full lg:hidden">
+          {/* <div className="w-full lg:hidden">
             <select
               className="w-full p-3 rounded-lg bg-gray-100 text-[#000080] font-extrabold"
               value={activeComponent || ""}
@@ -114,10 +114,10 @@ const CardOrganisationAscci = () => {
                 </option>
               ))}
             </select>
-          </div>
+          </div> */}
 
           {/* Zone de contenu */}
-          <div className="p-6 mt-4">
+          {/* <div className="p-6 mt-4">
             {ActiveComponent ? (
               <ActiveComponent />
             ) : (
@@ -125,7 +125,58 @@ const CardOrganisationAscci = () => {
                 <p>Sélectionnez une rubrique pour afficher son contenu</p>
               </div>
             )}
-          </div>
+          </div> */}
+
+          <div className="hidden md:flex flex-row justify-between items-center gap-2 text-black font-extrabold">
+  {Object.keys(CONTENT_COMPONENTS).map((id) => (
+    <Link
+      key={id}
+      href={`#${id}`}
+      className={`px-6 py-2 rounded-lg transition-all ${
+        activeComponent === id 
+          ? 'bg-[#000080] text-white'
+          : 'hover:bg-gray-100 text-[#000080]'
+      }`}
+      onClick={(e) => {
+        e.preventDefault();
+        handleLinkClick(id as keyof typeof CONTENT_COMPONENTS);
+      }}
+    >
+      {id.split('-').join(' ')}
+    </Link>
+  ))}
+</div>
+
+{/* Version mobile - Liste déroulante */}
+<div className="md:hidden w-full">
+  <select
+    className="w-full p-3 rounded-lg border-2 border-[#000080] text-[#000080] font-extrabold"
+    value={activeComponent || ''}
+    onChange={(e) => {
+      const selectedId = e.target.value as keyof typeof CONTENT_COMPONENTS;
+      handleLinkClick(selectedId);
+      window.location.hash = `#${selectedId}`;
+    }}
+  >
+    <option value="">Sélectionnez une rubrique</option>
+    {Object.keys(CONTENT_COMPONENTS).map((id) => (
+      <option key={id} value={id}>
+        {id.split('-').join(' ')}
+      </option>
+    ))}
+  </select>
+</div>
+
+{/* Zone de contenu */}
+<div className="p-6 mt-4">
+  {ActiveComponent ? (
+    <ActiveComponent />
+  ) : (
+    <div className="text-center py-10 text-gray-500">
+      <p>Sélectionnez une rubrique pour afficher son contenu</p>
+    </div>
+  )}
+</div>
         </div>
       </div>
     </div>
