96898ee905233ddac4fcfa3375e6f8c4c33cc57b
fabriceadou5@gmail.com <fabriceadou5@gmail.com>
2025-10-31 14:55:41 +0000

    optimisation de certaines interfaces update 1

--- app/components/OrganisationAscci/JeunesConseillers.tsx
+++ app/components/OrganisationAscci/JeunesConseillers.tsx
@@ -10,20 +10,20 @@ const JeunesConseillers = () => (
           <Image
             src={imagefille}
             width={300}
-            alt="Logo de l'ASCCI"
+            alt="Logo de l&apos;ASCCI"
             className="w-full rounded-2xl"
           />
         </div>
         <div className="w-full ">
           <div className="pl-4">
             Le Forum des Jeunes de l&apos;Association des Scouts Catholique de Côte
-            d&apos;Ivoire offre aux jeunes de l&apos;association la chance d'exprimer
+            d&apos;Ivoire offre aux jeunes de l&apos;association la chance d&apos;exprimer
             leurs points de vue&lsquo; de faire des recommandations concernant les
             sujets qui les intéressent&lsquo; et de développer les compétences
             nécessaires pour améliorer leur capacité à prendre part aux
             processus de prise de décision. Précédant l&apos;assemblé générale&lsquo; il
-            offre également aux délégués du Forum des Jeunes l'opportunité
-            d'écouter les points de vue des jeunes sur les questions qui les
+            offre également aux délégués du Forum des Jeunes l&apos;opportunité
+            d&apos;écouter les points de vue des jeunes sur les questions qui les
             concernent.
           </div>
         </div>
@@ -32,11 +32,11 @@ const JeunesConseillers = () => (
         Il a pour but d&apos;offrir une plateforme aux jeunes de l&apos;Ascci pour
         exprimer leurs opinions&lsquo; participer aux processus de prise de décision
         au sein de l&apos;association&lsquo; et développer leurs compétences et leurs
-        capacités de leadership&lsquo; pour finalement façonner l'avenir de
+        capacités de leadership&lsquo; pour finalement façonner l&apos;avenir de
         l&apos;association. Le Forum des jeunes prépare et propose des
         recommandations à l&apos;assemblé générale qui sont délibérées et&lsquo; si elles
         sont votées&lsquo; font partie des résolutions de l&apos;assemblée générale. En
-        outre&lsquo; le Forum des Jeunes offre l'opportunité aux délégués et aux
+        outre&lsquo; le Forum des Jeunes offre l&apos;opportunité aux délégués et aux
         observateurs de développer les compétences pratiques nécessaires pour
         leur permettre de participer plus efficacement aux processus de prise de
         décision et assumer des rôles de leadership à l&apos;intérieur et à
@@ -47,15 +47,15 @@ const JeunesConseillers = () => (
         <p>Ses objectifs sont de :</p>
         <ol className="list-decimal pl-5 mt-2 font-bold">
           <li>
-            Fournir un environnement d'apprentissage par le biais de discussions
-            sur des sujets d'intérêt et le partage d'expériences.
+            Fournir un environnement d&apos;apprentissage par le biais de discussions
+            sur des sujets d&apos;intérêt et le partage d&apos;expériences.
           </li>
           <li>
             Améliorer les capacités de prise de décision des participants pour
             l&apos;association et la société en général.
           </li>
           <li>
-            Développer l'amitié entre les participants et créer un réseau qui
+            Développer l&apos;amitié entre les participants et créer un réseau qui
             peut se poursuivre au-delà du Forum.
           </li>
           <li>
